e7200e12a54b721daed389490c08bec499c0f646
Dheeraj Rathee <sunnyrathee101@gmail.com>
2022-11-09 20:20:56 +0530

    Added Test Cases

--- src/app/fund-transfer/fund-transfer.component.spec.ts
+++ src/app/fund-transfer/fund-transfer.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms'
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FundTransferComponent } from './fund-transfer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('FundTransferComponent', () => {
   let component: FundTransferComponent;
@@ -8,9 +12,16 @@ describe('FundTransferComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FundTransferComponent ]
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        ToastrModule.forRoot()
+      ],
+      declarations: [FundTransferComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(FundTransferComponent);
     component = fixture.componentInstance;
@@ -19,5 +30,33 @@ describe('FundTransferComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be valid if form value is valid', () => {
+    component.fundTransferForm.setValue({
+      "sourceAccount": "1111",
+      "beneficiaryName": "Test",
+      "beneficiaryAccount": "2222",
+      "beneficiaryIfsc": "TEST176",
+      "beneficiaryAccountType": 1,
+      "amount": 200,
+      "remarks": "",
+      "acceptTerms": true
+    });
+    expect(component.fundTransferForm.valid).toEqual(true);
+  });
+
+  it('should be invalid if form value is invalid', () => {
+    component.fundTransferForm.setValue({
+      "sourceAccount": "1111",
+      "beneficiaryName": "Test",
+      "beneficiaryAccount": "2222",
+      "beneficiaryIfsc": "TEST176",
+      "beneficiaryAccountType": 1,
+      "remarks": "",
+      "amount": '',
+      "acceptTerms": true
+    });
+    expect(component.fundTransferForm.valid).toEqual(false);
   });
 });
--- src/app/fund-transfer/fund-transfer.service.spec.ts
+++ src/app/fund-transfer/fund-transfer.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { FundTransferService } from './fund-transfer.service';
 
 describe('FundTransferService', () => {
   let service: FundTransferService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,]
+    });
     service = TestBed.inject(FundTransferService);
   });
 
